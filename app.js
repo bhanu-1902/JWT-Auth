80dd048e8eefb5c2e4104f14ee807e58de7073c1
Harshit Bhanushali <harshit.21810372@viit.ac.in>
2021-08-06 11:54:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI = "mongodb://127.0.0.1/node-auth";
+//   "mongodb+srv://mongodb:mongodb@cluster0.4sljj.mongodb.net/node-auth";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
