730eba3c869fc59dd1ac9cd6bcf3a839a9a13f84
Harshit Bhanushali <harshit.21810372@viit.ac.in>
2021-08-07 15:28:25 +0530

    Mongoose hooks and Hash Password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,23 @@ const userSchema = new mongoose.Schema({
     required: [true, "Password is required"],
     minlength: [6, "Password should contain atleast 6 characters"],
   },
+});
+
+//Mongoose Hook - Fire a fn after an event has happened
+//Ex- fire a fn after doc has been saved to db
+// userSchema.post("save", function (doc, next) {
+//   //fires post doc has been saved
+//   console.log("New user created and saved", doc);
+//   next();
+// });
+
+//Ex- fire a fn before doc has been saved to db
+userSchema.pre("save", async function (next) {
+  //fires before doc has been saved
+  // console.log("New user about to be created and saved", this);
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
