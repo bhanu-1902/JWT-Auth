c30fe0487ce477675e69d237ee34454107af560b
Harshit Bhanushali <harshit.21810372@viit.ac.in>
2021-08-08 11:13:10 +0530

    Log user out

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,6 @@ router.get("/signup", authController.signup_get);
 router.get("/login", authController.login_get);
 router.post("/signup", authController.signup_post);
 router.post("/login", authController.login_post);
+router.get("/logout", authController.logout_get);
 
 module.exports = router;
